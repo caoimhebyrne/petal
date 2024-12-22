3e8cc04cdd91b6c594c2dbd5b1d7c2c75be5911a
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2024-12-22 16:21:17 +0000

    fix(ast): ensure that node(streams) are free'd properly

--- src/ast/node.c
+++ src/ast/node.c
@@ -7,7 +7,44 @@
 
 CREATE_STREAM(NodeStream, node_stream, Node*);
 
-void node_stream_destroy(NodeStream* stream) { free(stream->data); }
+void node_destroy(Node* node) {
+    switch (node->node_type) {
+    case NODE_VARIABLE_DECLARATION: {
+        VariableDeclarationNode* variable_declaration = (VariableDeclarationNode*)node;
+        free(variable_declaration->name);
+        free(variable_declaration->type_name);
+        node_destroy(variable_declaration->value);
+
+        break;
+    }
+
+    case NODE_FUNCTION_DECLARATION: {
+        FunctionDeclarationNode* function_declaration = (FunctionDeclarationNode*)node;
+        free(function_declaration->name);
+        node_stream_destroy(&function_declaration->function_body);
+
+        break;
+    }
+
+    case NODE_NUMBER_LITERAL:
+        break;
+
+    case NODE_IDENTIFIER_REFERENCE: {
+        IdentifierReferenceNode* identifier_reference = (IdentifierReferenceNode*)node;
+        free(identifier_reference->name);
+
+        break;
+    }
+    }
+}
+
+void node_stream_destroy(NodeStream* stream) {
+    for (size_t i = 0; i < stream->length; i++) {
+        node_destroy(stream->data[i]);
+    }
+
+    free(stream->data);
+}
 
 char* node_to_string(Node* node) {
     switch (node->node_type) {
