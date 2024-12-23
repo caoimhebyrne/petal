15f0f6ea13b092d4eb9afa51b038139cadc67c8b
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2024-12-23 22:41:05 +0000

    build: use llvm-config to get llvm cflags

--- src/position.h
+++ src/position.h
@@ -14,19 +14,8 @@ typedef struct Position {
     size_t index;
 } Position;
 
-inline void position_advance(Position* position) {
-    position->column += 1;
-    position->index += 1;
-}
-
-inline void position_retreat(Position* position) {
-    position->column -= 1;
-    position->index -= 1;
-}
-
-inline void position_advance_line(Position* position) {
-    position->column = 0;
-    position->line += 1;
-}
+void position_advance(Position* position);
+void position_retreat(Position* position);
+void position_advance_line(Position* position);
 
 #endif // __POSITION_H__
