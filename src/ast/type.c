0c1104f8ab387955968474f32e3c42f5d47a1c14
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2024-12-26 12:36:23 +0000

    fix(type): print pointer types properly

--- src/ast/type.c
+++ src/ast/type.c
@@ -1,4 +1,5 @@
 #include "type.h"
+#include "../string/format_string.h"
 #include <string.h>
 
 Type type_create(TypeKind kind, bool is_pointer) { return (Type){kind, is_pointer}; }
@@ -18,23 +19,33 @@ TypeKind type_kind_from_string(char* value) {
 }
 
 char* type_to_string(Type type) {
+    char* type_name = "unknown type";
+
     switch (type.kind) {
     case TYPE_KIND_INVALID:
-        return "invalid type";
+        type_name = "invalid type";
+        break;
 
     case TYPE_KIND_INT_32:
-        return "i32";
+        type_name = "i32";
+        break;
 
     case TYPE_KIND_INT_64:
-        return "i64";
+        type_name = "i64";
+        break;
 
     case TYPE_KIND_FLOAT_32:
-        return "f32";
+        type_name = "f32";
+        break;
 
     case TYPE_KIND_VOID:
-        return "void";
+        type_name = "void";
+        break;
     }
 
-    // This should never happen, the compiler is being weird.
-    return "unknown type";
+    if (type.is_pointer) {
+        return format_string("*%s", type_name);
+    }
+
+    return type_name;
 }
