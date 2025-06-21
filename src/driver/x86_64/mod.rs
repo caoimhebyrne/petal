9d639140a642ebaff3bded598b6e89c1c852b46c
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2025-06-21 17:51:10 +0100

    chore(driver/x86_64): use libc

--- src/driver/x86_64/mod.rs
+++ src/driver/x86_64/mod.rs
@@ -34,25 +34,15 @@ impl Driver for X86_64Driver {
             self.compile_function(function, &mut code);
         }
 
-        code.push_str("\n.global _start\n");
-        code.push_str("_start:\n");
-        code.push_str("    call main\n");
-        code.push_str("    mov edi, eax\n");
-        code.push_str("    mov rax, 60\n");
-        code.push_str("    syscall\n");
-
         let assembly_file = self.output_path.with_extension("s");
         let assembly_file_str = assembly_file.to_str().expect("Invalid path?");
-
-        let object_path = self.output_path.with_extension("o");
-        let object_path_str = object_path.to_str().expect("Invalid path?");
 
         fs::write(&assembly_file, code).map_err(|_| DriverError::IOError {
             file_name: assembly_file_str.to_owned(),
         })?;
 
-        let compile_output = Command::new("as")
-            .args(["-mintel64", assembly_file_str, "-o", object_path_str])
+        let compile_output = Command::new("gcc")
+            .args([assembly_file_str, "-o", self.output_path.to_str().unwrap()])
             .output()
             .map_err(|_| DriverError::CompilationFailure)?;
 
@@ -61,18 +51,6 @@ impl Driver for X86_64Driver {
             let _ = stderr().write_all(&compile_output.stderr);
 
             return Err(DriverError::CompilationFailure);
-        }
-
-        let link_output = Command::new("ld")
-            .args(["-o", self.output_path.to_str().unwrap(), object_path_str])
-            .output()
-            .map_err(|_| DriverError::LinkingFailure)?;
-
-        if !link_output.status.success() {
-            let _ = stdout().write_all(&link_output.stdout);
-            let _ = stderr().write_all(&link_output.stderr);
-
-            return Err(DriverError::LinkingFailure);
         }
 
         Ok(())
@@ -85,7 +63,7 @@ impl X86_64Driver {
         let stack_size_aligned = (stack_size + 15) & !15;
 
         // This is the start of every function.
-        code.push_str(&format!("{}:\n", function.name));
+        code.push_str(&format!(".global {}\n{}:\n", function.name, function.name));
 
         code.push_str("    push rbp\n");
         code.push_str("    mov rbp, rsp\n");
