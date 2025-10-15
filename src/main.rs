410468021cce1f93e64eb213dce5cba6fd14dd19
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2025-10-15 18:10:03 +0100

    feat: read file path from command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use crate::lexer::Lexer;
+use std::{env, fs, process};
+
 pub mod core;
 pub mod lexer;
 
+/// This is the entrypoint for the Petal compiler.
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+
+    // The first argument to the program must always be the name of the binary.
+    let program_name = args.next().unwrap_or_else(|| "petal".into());
+
+    // Then, we must have a file path to read from.
+    let file_path = match args.next() {
+        Some(value) => value,
+
+        None => {
+            eprintln!("Usage: {} <file_path>", program_name);
+            process::exit(1);
+        }
+    };
+
+    let contents = match fs::read_to_string(&file_path) {
+        Ok(value) => value,
+
+        Err(error) => {
+            eprintln!("ERROR: Failed to read from file '{}': {}", file_path, error);
+            process::exit(1);
+        }
+    };
+
+    let mut _lexer = Lexer::new(&contents);
+
+    // TODO: Pass the Lexer to the AST generator,
 }
