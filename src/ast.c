e6a0e57ff3f50bb7c25841b985f532a96d110bd4
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2026-01-18 11:45:01 +0000

    chore(ast): add asserts to public functions

--- src/ast.c
+++ src/ast.c
@@ -1,17 +1,23 @@
 #include "ast.h"
 #include "array.h"
 #include "ast_statement.h"
+#include <assert.h>
 
 IMPLEMENT_ARRAY_TYPE(StatementArray, statement_array, Statement*)
 
 void ast_parser_init(ASTParser* parser, Module* module, const TokenArray* tokens) {
+    assert(parser != NULL && "NULL parser pointer passed to ast_parser_init");
+    assert(module != NULL && "NULL module pointer passed to ast_parser_init");
+    assert(tokens != NULL && "NULL tokens pointer passed to ast_parser_init");
+
     parser->tokens = tokens;
     parser->module = module;
     parser->cursor = 0;
 }
 
 bool ast_parser_parse(ASTParser* parser, StatementArray* statements) {
-    (void)parser;
-    (void)statements;
+    assert(parser != NULL && "NULL parser pointer passed to ast_parser_parse");
+    assert(statements != NULL && "NULL statements pointer passed to ast_parser_parse");
+
     return false;
 }
