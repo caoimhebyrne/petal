dbcd6ffddde68dc6c2b8aa4c0bfdcdb1d528c715
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2026-04-26 17:01:47 +0100

    chore(main): use color for steps and exit on failure

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::{
     env,
     ffi::OsStr,
     path::PathBuf,
+    process::ExitCode,
 };
+
+use owo_colors::OwoColorize;
 
 use crate::{
     backend::c::CBackend,
@@ -16,7 +19,7 @@ pub mod core;
 pub mod lexer;
 pub mod module;
 
-fn main() {
+fn main() -> ExitCode {
     let mut args = env::args();
 
     let program_name = args.next().unwrap_or("petal".into());
@@ -25,46 +28,49 @@ fn main() {
         Some(value) => value,
         _ => {
             eprintln!("Usage: {} file_path", program_name);
-            return;
+            return ExitCode::FAILURE;
         }
     };
 
-    println!("[1/4] Creating module from '{file_path}'");
+    println!("{} Creating module from '{file_path}'", "[1/4]".bright_purple());
 
     let module = match Module::create(file_path.clone()) {
         Ok(value) => value,
         Err(error) => {
             eprintln!("error: {}", error);
-            return;
+            return ExitCode::FAILURE;
         }
     };
 
-    println!("[2/4] Parsing module");
+    println!("{} Parsing module", "[2/4]".bright_purple());
 
     let parsed_module = match module.parse() {
         Ok(value) => value,
         Err(error) => {
             error.print_to_stderr(&module.file_path, &module.file_contents);
-            return;
+            return ExitCode::FAILURE;
         }
     };
 
-    println!("[3/4] Generating C code");
+    println!("{} Generating C code", "[3/4]".bright_purple());
 
     let code = match CBackend::emit_code(&parsed_module) {
         Ok(value) => value,
         Err(error) => {
             error.print_to_stderr(&module.file_path, &module.file_contents);
-            return;
+            return ExitCode::FAILURE;
         }
     };
 
     // `./path/to/petal/file.petal` -> `file`
     let binary_file_name = PathBuf::from(file_path).file_stem().and_then(OsStr::to_str).unwrap_or("output").to_string();
 
-    println!("[4/4] Compiling binary ('{binary_file_name}')");
+    println!("{} Compiling binary ('{binary_file_name}')", "[4/4]".bright_purple());
 
     if let Err(error) = CBackend::emit_binary(&code, binary_file_name) {
         error.print_to_stderr(&module.file_path, &module.file_contents);
+        return ExitCode::FAILURE;
     }
+
+    ExitCode::SUCCESS
 }
